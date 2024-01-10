5b6a3ab9feae4ce871e6ece972eb2b963b8133b8
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-01-10 10:00:04 +0100

    title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>WEATHER MASTER WELCOMES YOU TO THIS MAGNIFICANT APP</h1>
+      <p>by Netti and the Philipps (c)</p>
+    </div>
+  );
 }
 
 export default App;
